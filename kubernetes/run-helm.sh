45dfd14f125e31069dea0d9e97077ad3f000d115
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2019-05-21 12:06:13 -0300

    Atualização de saída

--- kubernetes/run-helm.sh
+++ kubernetes/run-helm.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-docker build -t webserver:1.0 . && helm init
+minikube status | grep -i "running" -m 1
+
+if [ $? -ne 0 ]
+then
+        minikube start
+fi
+
+echo -e "\n[ Construindo imagem local... ]\n"
+docker build -t webserver:1.0 . && helm init --upgrade
+echo -e "\n[ Instalando a aplicação... ]\n"
 helm install --name devops ./app-idwaller/ --namespace qa-idwall
