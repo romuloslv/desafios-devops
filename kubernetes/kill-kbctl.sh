b2a1422610dd92a87711351310aca46e8b12f616
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2019-05-21 12:07:13 -0300

    Atualização de saída

--- kubernetes/kill-kbctl.sh
+++ kubernetes/kill-kbctl.sh
@@ -4,6 +4,7 @@ kubectl get namespace | grep -i "qa-idwall"
 
 if [ $? -ne 1 ]
 then
+	echo -e "\n[ Removendo configuração... ]\n"
 	kubectl delete -f manifestos/. --namespace=qa-idwall
 	docker rmi -f webserver:1.0
 fi
