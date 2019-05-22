b06e813daa7bb3e48612a410fd0cc54788c52c03
Rômulo Ferreira <romuloslv@users.noreply.github.com>
2019-05-22 17:16:40 -0300

    Remoção através de namespace

--- kubernetes/kill-kbctl.sh
+++ kubernetes/kill-kbctl.sh
@@ -5,7 +5,7 @@ kubectl get namespace | grep -i "qa-idwall"
 if [ $? -ne 1 ]
 then
 	echo -e "\n[ Removendo configuração... ]\n"
-	kubectl delete -f manifestos/. --namespace=qa-idwall
+	kubectl delete namespace qa-idwall
 	docker rmi -f webserver:1.0
 fi
 
